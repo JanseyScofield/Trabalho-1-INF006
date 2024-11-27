dc9eca721058f1f6caf859b5d9fc8058577aaa21
Jansey Scofield <janscofield10@gmail.com>
2024-11-26 23:02:19 -0300

    Fazendo estrutura basica de leitura e escrita de arquivos

--- L0Q1.c
+++ L0Q1.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define TAM_MAX_LINHA 1000
 
 typedef struct{
     char *coordenadaPonto;
     float distanciaOrigem;
 } Ponto;
 
+int main(){
+    // Armazenando os arquivos de entrada e saida em variaveis:
+    FILE *entrada = fopen("L0Q1.in", "r");
+    FILE *saida = fopen("L0Q1.out", "w");
+
+    // Validando se foi possivel acessa-los:
+    if(entrada == NULL || saida == NULL){
+        printf("Nao foi possivel carregar os arquivos de entrada e saida.\n");
+        return 0;
+    }
+    //Escrevendo o conteudo de um arquivo no outro:
+    char *linha = malloc(TAM_MAX_LINHA * sizeof(char));
+
+    while(fgets(linha, TAM_MAX_LINHA,  entrada) != NULL){
+        fputs(linha, saida);
+    }
+
+    fclose(entrada);
+    fclose(saida);
+
+    return 1;
+}
